342d1f51e332a81e2f34f1ec371d92bf73a863fb
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-02-01 21:37:02 +0300

    added some reusable ui components / edit some global styles

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,15 @@
+import LoaderImg from '@assets/icons/loader.svg';
+
+const Loader = () => (
+	<div className="flex-center w-full">
+		<img
+			src={LoaderImg}
+			alt="loader"
+			width={24}
+			height={24}
+			className="animate-spin"
+		/>
+	</div>
+);
+
+export default Loader;
