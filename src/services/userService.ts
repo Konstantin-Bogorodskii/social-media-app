d74eec8cb706afdd30a29f4488014d783aec3d45
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-02-02 18:48:35 +0300

    created userService and added two functions for creating user account and for saving user to database

---
+++ src/services/userService.ts
@@ -0,0 +1,54 @@
+import { ID } from 'appwrite';
+import { account, avatars, databases } from '@lib/appwrite/config';
+import { INewUser } from '@/types/types';
+import { appwriteConfig } from './../lib/appwrite/config';
+
+const userService = {
+	createUserAccount
+};
+
+export default userService;
+
+async function createUserAccount(user: INewUser) {
+	try {
+		const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+
+		if (!newAccount) throw Error;
+
+		const avatarUrl = avatars.getInitials(user.name);
+
+		const newUser = await saveUserToDB({
+			accountId: newAccount.$id,
+			name: newAccount.name,
+			email: newAccount.email,
+			username: user.username,
+			imageUrl: avatarUrl
+		});
+
+		return newUser;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
+
+async function saveUserToDB(user: {
+	accountId: string;
+	email: string;
+	name: string;
+	imageUrl: URL;
+	username?: string;
+}) {
+	try {
+		const newUser = await databases.createDocument(
+			appwriteConfig.databaseId,
+			appwriteConfig.usersCollectionId,
+			ID.unique(),
+			user
+		);
+
+		return newUser;
+	} catch (error) {
+		console.log(error);
+	}
+}
