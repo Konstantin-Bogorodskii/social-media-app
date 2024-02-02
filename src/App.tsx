c69e75cc7aac023ba5744895d3e8f5dede3d8982
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-02-02 18:44:39 +0300

    moved router provider from main.tsx to App.tsx component / added Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
+import router from '@/router/router';
+
+import { Toaster } from '@/components/ui/toaster';
+
 const App: React.FC = () => {
-	return <main className="flex h-screen"></main>;
+	return (
+		<>
+			<main className="flex h-screen">
+				<RouterProvider router={router} />
+			</main>
+			<Toaster />
+		</>
+	);
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { RouterProvider } from 'react-router-dom';
-import router from '@/router/router';
-
+import App from '@/App';
 import '@styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>
 );
