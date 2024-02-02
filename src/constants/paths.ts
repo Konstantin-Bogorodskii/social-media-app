937165b62e0ed38beb93c7371dc965778ae7e726
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-02-02 18:43:28 +0300

    changed paths constants declaration from const to enum

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,7 +1,7 @@
-const PATHS = {
-	ROOT: '/',
-	SIGN_IN: '/sign-in',
-	SIGN_UP: '/sign-up'
-} as const;
+enum PATHS {
+	ROOT = '/',
+	SIGN_IN = '/sign-in',
+	SIGN_UP = '/sign-up'
+}
 
 export default PATHS;
